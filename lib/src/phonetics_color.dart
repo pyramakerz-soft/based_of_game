fec255f820a259d8985eb0923cd6938b966bae6b
moatazmohamed <mohamed.moataz194@gmail.com>
2024-06-07 18:04:52 +0300

    missed files

--- lib/src/phonetics_color.dart
+++ lib/src/phonetics_color.dart
@@ -2,5 +2,5 @@ import 'package:flutter/cupertino.dart';
 
 class AppColorPhonetics {
   static const Color boarderColor = Color(0xFF54A2B7);
-  static const Color DarkBorderColor = Color(0xFF114677);
+  static const Color darkBorderColor = Color(0xFF114677);
 }

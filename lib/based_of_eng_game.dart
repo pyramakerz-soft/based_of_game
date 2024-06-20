d9710867a3769bd5da2fcbe303aef48e0d67b721
Moataz Mohamed <mohamed.moataz194@gmail.com>
2024-06-20 10:29:04 +0300

    basic_of_game_data

--- lib/based_of_eng_game.dart
+++ lib/based_of_eng_game.dart
@@ -4,3 +4,4 @@ export 'src/widgets/stroke_text.dart';
 export 'src/pages/main_screen.dart';
 export 'src/core/games_structure/basic_of_chapter.dart';
 export 'src/core/games_structure/base_of_games.dart';
+export 'src/core/games_structure/basic_of_game_data.dart';

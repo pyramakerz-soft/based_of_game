ac525a80969937d258d39a0b4e7dd971f035177c
moatazmohamed <mohamed.moataz194@gmail.com>
2024-07-09 22:20:41 +0300

    bingo

--- lib/src/games/bingo_game/widget/item_card_widget.dart
+++ lib/src/games/bingo_game/widget/item_card_widget.dart
@@ -25,48 +25,42 @@ class ItemCardWidget extends StatelessWidget {
     return SizedBox(
       width: maxWidth / 3,
       height: maxHeight / 3,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          GestureDetector(
-            onTap: onTap,
-            child: Container(
-              // width: maxWidth / 3,
-              // height: maxHeight / 3,
-              // padding: const EdgeInsets.only(top: 8, left: 8, right: 8, bottom: 36),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                // shape: RoundedRectangleBorder(
-                //   side: BorderSide(width: 2, Color(0xFF91D4F0)),
-                // ),
-                border: Border(
-                  right: (index == 0 || index == 3 || index == 6)
-                      ? const BorderSide(width: 2, color: Color(0xFF91D4F0))
-                      : const BorderSide(color: Colors.white),
-                  left: (index == 2 || index == 5 || index == 8)
-                      ? const BorderSide(width: 2, color: Color(0xFF91D4F0))
-                      : const BorderSide(color: Colors.white),
-                  top: (index == 6 || index == 7 || index == 8)
-                      ? const BorderSide(width: 2, color: Color(0xFF91D4F0))
-                      : const BorderSide(color: Colors.white),
-                  bottom: (index == 0 || index == 1 || index == 2)
-                      ? const BorderSide(width: 2, color: Color(0xFF91D4F0))
-                      : const BorderSide(color: Colors.white),
-                ),
-              ),
-              alignment: Alignment.center,
-              child: StrokeText(text: body, isDisabled: false, fontSize: 20.sp),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          // width: maxWidth / 3,
+          // height: maxHeight / 3,
+          // padding: const EdgeInsets.only(top: 8, left: 8, right: 8, bottom: 36),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            // shape: RoundedRectangleBorder(
+            //   side: BorderSide(width: 2, Color(0xFF91D4F0)),
+            // ),
+            border: Border(
+              right: (index == 0 || index == 3 || index == 6)
+                  ? const BorderSide(width: 2, color: Color(0xFF91D4F0))
+                  : const BorderSide(color: Colors.white),
+              left: (index == 2 || index == 5 || index == 8)
+                  ? const BorderSide(width: 2, color: Color(0xFF91D4F0))
+                  : const BorderSide(color: Colors.white),
+              top: (index == 6 || index == 7 || index == 8)
+                  ? const BorderSide(width: 2, color: Color(0xFF91D4F0))
+                  : const BorderSide(color: Colors.white),
+              bottom: (index == 0 || index == 1 || index == 2)
+                  ? const BorderSide(width: 2, color: Color(0xFF91D4F0))
+                  : const BorderSide(color: Colors.white),
             ),
           ),
-          if (hide == true) ...{
-            Container(
-              height: 50,
-              decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: AppColorPhonetics.darkBorderColor),
-            )
-          }
-        ],
+          alignment: Alignment.center,
+          child: (hide == true)
+              ? Container(
+                  height: 50,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: AppColorPhonetics.darkBorderColor),
+                )
+              : StrokeText(text: body, isDisabled: false, fontSize: 20.sp),
+        ),
       ),
     );
   }

b9b0c7d8023b5544e4680c2864a112c4e386c798
moatazmohamed <mohamed.moataz194@gmail.com>
2024-07-09 21:03:22 +0300

    ClickTheWordScreen

--- lib/src/games/click_the_word/screen/click_the_word_screen.dart
+++ lib/src/games/click_the_word/screen/click_the_word_screen.dart
@@ -43,140 +43,151 @@ class _ClickTheWordScreen extends State<ClickTheWordScreen> {
 
     return BlocConsumer<ClickTheWordCubit, ClickTheWordInitial>(
         listener: (context, state) {
+      context.read<CurrentGamePhoneticsCubit>().saveTheStringWillSay(
+          stateOfStringIsWord: StateOfSubWord.isWord,
+          stateOfStringWillSay: state.gameData.correctAns ?? '');
+    }, builder: (context, gameState) {
+      return Container(
+        margin: const EdgeInsets.only(bottom: (15 + 50), top: 50),
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        width: MediaQuery.of(context).size.width - (130 + 50 + 130),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            border:
+                Border.all(color: AppColorPhonetics.boarderColor, width: 5)),
+        child: Column(
+          children: [
+            MasterWord(word: gameState.headOfQuestion ?? ''),
+            25.ph,
+            Row(
+                mainAxisSize: MainAxisSize.min,
+                children: List.generate(
+                    gameState.choose?.length ?? 0,
+                    (index) => Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 5),
+                        child: GestureDetector(
+                          onTap: () async {
+                            List<int> listCorrectAnswers =
+                                gameState.listCorrectAnswers;
+                            int countOfQuestion = 0;
+                            List<GameFinalModel> listGameData2 = gameState
+                                .listGameData
+                                .sublist(0, (gameState.index + 1));
+                            listGameData2.forEach((element) {
+                              int countOfQuestionSubGame = element.gameLetters
+                                      ?.where((element2) =>
+                                          element2.letter == element.correctAns)
+                                      .length ??
+                                  0;
+                              countOfQuestion =
+                                  countOfQuestion + countOfQuestionSubGame;
+                            });
+                            if (context
+                                    .read<CurrentGamePhoneticsCubit>()
+                                    .ableButton() &&
+                                (!gameState.listCorrectAnswers
+                                    .contains(gameState.choose?[index].id)) &&
+                                (listCorrectAnswers.length !=
+                                    countOfQuestion)) {
+                              if ((gameState.choose?[index].letter ==
+                                  gameState.headOfQuestion)) {
+                                bool stateOfAnswer = context
+                                    .read<ClickTheWordCubit>()
+                                    .addAnswer(
+                                        answerId:
+                                            gameState.choose?[index].id ?? 0);
+                                await context
+                                    .read<CurrentGamePhoneticsCubit>()
+                                    .addSuccessAnswer(
+                                        questions: gameState.countQuestion ?? 0,
+                                        correctAnswers:
+                                            gameState.listCorrectAnswers.length)
+                                    .whenComplete(() async {
+                                  print('stateOfAnswer:$stateOfAnswer');
+                                  if (stateOfAnswer == true) {
+                                    // bool isLastQuestion = context
+                                    //     .read<
+                                    //         CurrentGamePhoneticsCubit>()
+                                    //     .checkIfIsTheLastQuestionOfGame(
+                                    //         queations: gameState
+                                    //                 .countQuestion ??
+                                    //             0);
+                                    print(
+                                        'countQuestion:${gameState.countQuestion}, ${gameState.listCorrectAnswers.length}');
 
-          context.read<CurrentGamePhoneticsCubit> ().saveTheStringWillSay(
-              stateOfStringIsWord: StateOfSubWord.isWord,
-              stateOfStringWillSay: state.gameData.correctAns ?? '');
-        },
-        builder: (context, gameState) {
-          return Container(
-            margin: const EdgeInsets.only(bottom: (15 + 50), top: 50),
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-            width: MediaQuery.of(context).size.width - (130 + 50 + 130),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-                border: Border.all(
-                    color: AppColorPhonetics.boarderColor, width: 5)),
-            child: Column(
-              children: [
-               MasterWord(word: gameState.headOfQuestion ?? ''),
-                25.ph,
-                Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: List.generate(
-                        gameState.choose?.length ?? 0,
-                        (index) => Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 5),
-                            child: GestureDetector(
-                              onTap: () async {
-                                if (context
-                                        .read<CurrentGamePhoneticsCubit>()
-                                        .ableButton() &&
-                                    (!gameState.listCorrectAnswers.contains(
-                                        gameState.choose?[index].id))) {
-                                  if ((gameState.choose?[index].letter ==
-                                      gameState.headOfQuestion)) {
-                                    bool stateOfAnswer = context
-                                        .read<ClickTheWordCubit>()
-                                        .addAnswer(
-                                            answerId:
-                                                gameState.choose?[index].id ??
-                                                    0);
-                                    await context
-                                        .read<CurrentGamePhoneticsCubit>()
-                                        .addSuccessAnswer(
-                                            questions:
-                                                gameState.countQuestion ?? 0,
-                                            correctAnswers: gameState
-                                                .listCorrectAnswers.length)
-                                        .whenComplete(() async {
-                                      print('stateOfAnswer:$stateOfAnswer');
-                                      if (stateOfAnswer == true) {
-                                        // bool isLastQuestion = context
-                                        //     .read<
-                                        //         CurrentGamePhoneticsCubit>()
-                                        //     .checkIfIsTheLastQuestionOfGame(
-                                        //         queations: gameState
-                                        //                 .countQuestion ??
-                                        //             0);
-                                        print(
-                                            'countQuestion:${gameState.countQuestion}, ${gameState.listCorrectAnswers.length}');
+                                    // print('isLastQuestion:$isLastQuestion');
 
-                                        // print('isLastQuestion:$isLastQuestion');
+                                    Future.delayed(const Duration(seconds: 2),
+                                        () async {
+                                      if (gameState.countQuestion ==
+                                          gameState.listCorrectAnswers.length) {
+                                        context
+                                            .read<CurrentGamePhoneticsCubit>()
+                                            .state
+                                            .actionWhenTriesBeZero
+                                            .call(context
+                                                    .read<
+                                                        CurrentGamePhoneticsCubit>()
+                                                    .state
+                                                    .countOfStar ??
+                                                0);
+                                        Navigator.of(context).pop();
 
-                                        Future.delayed(
-                                            const Duration(seconds: 2),
-                                            () async {
-                                          if (gameState.countQuestion ==
-                                              gameState
-                                                  .listCorrectAnswers.length) {
-                                            context
-                                                .read<
-                                                    CurrentGamePhoneticsCubit>()
-                                                .state
-                                                .actionWhenTriesBeZero
-                                                .call(context
-                                                        .read<
-                                                            CurrentGamePhoneticsCubit>()
-                                                        .state
-                                                        .countOfStar ??
-                                                    0);
-                                            Navigator.of(context).pop();
-
-                                            return;
-                                          }
-                                          await context
-                                              .read<CurrentGamePhoneticsCubit>()
-                                              .updateIndexOfCurrentGame();
-                                          context
-                                              .read<ClickTheWordCubit>()
-                                              .updateTheCurrentGame(
-                                                  index: context
-                                                      .read<
-                                                          CurrentGamePhoneticsCubit>()
-                                                      .state
-                                                      .index);
-                                        });
+                                        return;
                                       }
-                                      //  else {
-                                      //   Future.delayed(
-                                      //       const Duration(seconds: 2),
-                                      //       () async {
-                                      //
-                                      //   });
-                                      // }
+                                      await context
+                                          .read<CurrentGamePhoneticsCubit>()
+                                          .updateIndexOfCurrentGame();
+                                      context
+                                          .read<ClickTheWordCubit>()
+                                          .updateTheCurrentGame(
+                                              index: context
+                                                  .read<
+                                                      CurrentGamePhoneticsCubit>()
+                                                  .state
+                                                  .index);
                                     });
-                                  } else {
-                                    context
-                                        .read<ClickTheWordCubit>()
-                                        .addWrongAnswer(
-                                            answerId:
-                                                gameState.choose?[index].id ??
-                                                    0);
-                                    await context
-                                        .read<CurrentGamePhoneticsCubit>()
-                                        .addWrongAnswer(
-                                            actionOfWrongAnswer: () async {});
                                   }
-                                }
-                              },
-                              child: gameState.choose?[index].id ==
-                                      gameState.wrongAnswer
-                                  ? WrongWord(word: gameState.choose?[index].letter ?? '')
-                                  : gameState.listCorrectAnswers
-                                          .contains(gameState.choose?[index].id)
-                                      ? CorrectWord(
-                                          word:
-                                              gameState.choose?[index].letter ??
-                                                  '')
-                                      : ChooseWord(word: gameState.choose?[index].letter ??
-                                  '',),
-                            ))))
-              ],
-            ),
-          );
-        });
+                                  //  else {
+                                  //   Future.delayed(
+                                  //       const Duration(seconds: 2),
+                                  //       () async {
+                                  //
+                                  //   });
+                                  // }
+                                });
+                              } else {
+                                context
+                                    .read<ClickTheWordCubit>()
+                                    .addWrongAnswer(
+                                        answerId:
+                                            gameState.choose?[index].id ?? 0);
+                                await context
+                                    .read<CurrentGamePhoneticsCubit>()
+                                    .addWrongAnswer(
+                                        actionOfWrongAnswer: () async {});
+                              }
+                            }
+                          },
+                          child: gameState.choose?[index].id ==
+                                  gameState.wrongAnswer
+                              ? WrongWord(
+                                  word: gameState.choose?[index].letter ?? '')
+                              : gameState.listCorrectAnswers
+                                      .contains(gameState.choose?[index].id)
+                                  ? CorrectWord(
+                                      word:
+                                          gameState.choose?[index].letter ?? '')
+                                  : ChooseWord(
+                                      word:
+                                          gameState.choose?[index].letter ?? '',
+                                    ),
+                        ))))
+          ],
+        ),
+      );
+    });
   }
 }
